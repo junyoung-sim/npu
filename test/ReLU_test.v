9ad572c779c978be1abffa241687cc04298e5843
junyoung-sim <jysim.ny@gmail.com>
2025-04-19 01:09:24 -0400

    2025-04-19

--- test/ReLU_test.v
+++ test/ReLU_test.v
@@ -1,4 +1,4 @@
-`include "???.v"
+`include "ReLU.v"
 `include "TestUtils.v"
 
 module Top();
@@ -17,30 +17,38 @@ module Top();
   // DUT
   //==========================================================
 
-  /*
-    I/Os
-  */
+  logic [15:0] relu_dut_in;
+  logic [15:0] relu_dut_out;
 
-  /*
-    DUT
-  */
+  ReLU #(16) relu_dut
+  (
+    .in  (relu_dut_in),
+    .out (relu_dut_out)
+  );
 
   //==========================================================
   // Verification Tasks
   //==========================================================
 
+  function automatic logic signed [15:0] rand_q8_8();
+    return $urandom() & ((1 << 16) - 1);
+  endfunction
+
   task check
   (
-    input logic [] 
+    input logic [15:0] relu_in,
+    input logic [15:0] relu_out
   );
+
+    relu_dut_in = relu_in;
 
     if(!t.failed) begin
       #8;
       if(t.n != 0) begin
-        $display("%3d: ", t.cycles);
+        $display("%3d: %b.%b > %b.%b", t.cycles,
+          relu_in[15:8], relu_in[7:0], relu_out[15:8], relu_out[7:0]);
       end
-      //`CHECK_EQ();
-      //`CHECK_EQ_HEX();
+      `CHECK_EQ(relu_dut_out, relu_out);
       #2;
     end
 
@@ -50,11 +58,38 @@ module Top();
   // Test Cases
   //==========================================================
 
+  logic signed [15:0] relu_in;
+  logic        [15:0] relu_out;
+
+  //----------------------------------------------------------
+
   task test_case_1_basic();
     t.test_case_begin("test_case_1_basic");
+
+    check('h0100, 'h0100);
+    check('hff00, 'h0000);
+
   endtask
 
   //----------------------------------------------------------
+
+  task test_case_2_random();
+    t.test_case_begin("test_case_2_random");
+
+    for(int i = 0; i < 10000; i++) begin
+
+      relu_in  = rand_q8_8();
+      
+      if(relu_in < 0)
+        relu_out = 0;
+      else
+        relu_out = relu_in;
+
+      check(relu_in, relu_out);
+
+    end
+
+  endtask
 
   //==========================================================
   // Main
@@ -64,6 +99,7 @@ module Top();
     t.test_bench_begin(`__FILE__);
 
     if((t.n <= 0) || (t.n == 1)) test_case_1_basic();
+    if((t.n <= 0) || (t.n == 2)) test_case_2_random();
 
     t.test_bench_end();
   end
